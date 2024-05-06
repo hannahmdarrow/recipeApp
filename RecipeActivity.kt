24daed23608dce70a1a338dde9328ddd0a7c704c
Roshni Kainthan <113396273+roshnikai@users.noreply.github.com>
2024-05-06 12:04:23 -0400

    working on back button

--- RecipeActivity.kt
+++ RecipeActivity.kt
@@ -2,6 +2,10 @@ package com.example.recipeapp
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RatingBar
+import android.widget.RatingBar.OnRatingBarChangeListener
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,12 +14,14 @@ class RecipeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recipe_activity)
         context = this
+
         var nameView = findViewById<TextView>(R.id.recipeName)
         var ingredientsView = findViewById<TextView>(R.id.ingredients)
         var instructionsView = findViewById<TextView>(R.id.instructions)
         var name = currentRecipe.getName()
         var ingredients = currentRecipe.getIngredients()
         var instructions = currentRecipe.getInstructions()
+
         nameView.text = "Recipe Selected: " + name
         var allIngredients = "Ingredients: "
         for (ingredient in ingredients) {
@@ -29,6 +35,28 @@ class RecipeActivity : AppCompatActivity() {
             allInstructions += "${(i + 1)}. ${instructions[i]}\n"
         }
         instructionsView.text = allInstructions
+
+        //set favorite recipe
+        var favRecipe = findViewById<CheckBox>(R.id.checkbox_sample)
+
+        //set rating
+        var ratingBar = findViewById<RatingBar>(R.id.ratingBar)
+        ratingBar.onRatingBarChangeListener = RatingListener()
+
+        //go back to homepage
+        var goBack: Button = findViewById<Button>(R.id.back)
+        goBack.setOnClickListener {
+            if (favRecipe.isChecked)
+                currentRecipe.setFavorite()
+            this.finish()
+        }
+
+    }
+
+    inner class RatingListener: OnRatingBarChangeListener {
+        override fun onRatingChanged(ratingBar: RatingBar?, rating: Float, fromUser: Boolean) {
+            currentRecipe.setRating(rating)
+        }
     }
     companion object {
         lateinit var context: Context
