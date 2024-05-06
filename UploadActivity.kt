460519abc886adcc0027233a6fe77ff70781a1aa
hannahmdarrow <73087484+hannahmdarrow@users.noreply.github.com>
2024-05-06 17:43:35 -0400

    Send recipe to firebase

--- UploadActivity.kt
+++ UploadActivity.kt
@@ -2,22 +2,24 @@ package com.example.recipeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.upload_activity)
+        setContentView(R.layout.activity_upload)
 
         //current recipe
         var recipe = Recipe(this)
 
         //edit texts
-        var recipeEdit = findViewById<EditText>(R.id.recipeName)
+        var nameEdit = findViewById<EditText>(R.id.recipeName)
         var ingredientEdit = findViewById<EditText>(R.id.ingredient)
         var instructionEdit = findViewById<EditText>(R.id.instruction)
 
@@ -27,18 +29,14 @@ class UploadActivity : AppCompatActivity() {
         var submit = findViewById<Button>(R.id.submit)
         var back = findViewById<Button>(R.id.back)
 
-        //recipe info
-        var name = recipeEdit.text.toString()
-        var ingredient = ingredientEdit.text.toString()
-        var instruction = instructionEdit.text.toString()
-
         //add info to recipe
-        recipe.setName(name)
         addIngredient.setOnClickListener {
+            var ingredient = ingredientEdit.text.toString()
             recipe.addIngredient(ingredient)
             ingredientEdit.setText("", TextView.BufferType.EDITABLE)
         }
         addInstruction.setOnClickListener {
+            var instruction = instructionEdit.text.toString()
             recipe.addStep(instruction)
             instructionEdit.setText("", TextView.BufferType.EDITABLE)
         }
@@ -49,10 +47,12 @@ class UploadActivity : AppCompatActivity() {
         //submit recipe
         submit.setOnClickListener {
             //firebase reference
-            var recipeReference = FirebaseDatabase.getInstance(
-                "https://recipeapp-bbdaf-default-rtdb.firebaseio.com/").reference.child(name)
-            recipeReference.child("ingredients").setValue(recipe.getIngredients())
-            recipeReference.child("instructions").setValue(recipe.getInstructions())
+            var name = nameEdit.text.toString()
+            recipe.setName(name)
+            var database = FirebaseDatabase.getInstance()
+            var reference : DatabaseReference = database.reference
+            reference.child(name).child("ingredients").setValue(recipe.getIngredients())
+            reference.child(name).child("instructions").setValue(recipe.getInstructions())
             this.finish()
         }
     }
