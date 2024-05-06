c241221acfb2e09c53aaa2048d44cf1be9cf5168
hannahmdarrow <73087484+hannahmdarrow@users.noreply.github.com>
2024-05-06 17:45:24 -0400

    Increase legibility in onCreate

--- MainActivity.kt
+++ MainActivity.kt
@@ -18,6 +18,8 @@ import java.lang.reflect.Type
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var intent : Intent
+    private lateinit var database : FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,47 +28,49 @@ class MainActivity : AppCompatActivity() {
         val adapter: ArrayAdapter<*> =
             ArrayAdapter.createFromResource(this, R.array.Options, R.layout.spinner_item)
         spinner.adapter = adapter
-        var myIntent: Intent = Intent(this, UploadActivity::class.java)
-        var firebase = FirebaseDatabase.getInstance()
-        spinner.onItemSelectedListener = object :
-            AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>,
-                                        view: View, position: Int, id: Long) {
-                val options = resources.getStringArray(R.array.Options)
-                if (options[position] == "Search for recipe") {
-                    myIntent = Intent(this@MainActivity, SearchActivity::class.java)
-                } else if (options[position] == "Favorite recipe") {
-                    val pref = this@MainActivity.getSharedPreferences(
-                        this@MainActivity.packageName + "_preferences", Context.MODE_PRIVATE)
-                    if (pref.getString("favRecipe", "") != "") {
-                        var recipeName = pref.getString("favRecipe", "")
-                        var recipeChild = firebase.reference.child(recipeName!!)
-                        RecipeActivity.currentRecipe.setName(recipeName)
-                        recipeChild.addListenerForSingleValueEvent (object : ValueEventListener {
-                            override fun onDataChange(snapshot: DataSnapshot) {
-                                if (snapshot.exists()) {
-                                    RecipeActivity.currentRecipe.setIngredients(
-                                        snapshot.child("ingredients").value as ArrayList<String>)
-                                    RecipeActivity.currentRecipe.setInstructions(
-                                        snapshot.child("instructions").value as ArrayList<String>)
-                                }
+        intent = Intent(this, UploadActivity::class.java)
+        database = FirebaseDatabase.getInstance()
+        spinner.onItemSelectedListener = SpinnerListener()
+        start.setOnClickListener {
+            startActivity(intent)
+        }
+    }
+    
+    inner class SpinnerListener : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            val options = resources.getStringArray(R.array.Options)
+            if (options[p2] == "Search for recipe") {
+                intent = Intent(this@MainActivity, SearchActivity::class.java)
+            } else if (options[p2] == "Favorite recipe") {
+                val pref = this@MainActivity.getSharedPreferences(
+                    this@MainActivity.packageName + "_preferences", Context.MODE_PRIVATE)
+                if (pref.getString("favRecipe", "") != "") {
+                    var recipeName = pref.getString("favRecipe", "")
+                    var recipeChild = database.reference.child(recipeName!!)
+                    RecipeActivity.currentRecipe.setName(recipeName)
+                    recipeChild.addListenerForSingleValueEvent (object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            if (snapshot.exists()) {
+                                RecipeActivity.currentRecipe.setIngredients(
+                                    snapshot.child("ingredients").value as ArrayList<String>)
+                                RecipeActivity.currentRecipe.setInstructions(
+                                    snapshot.child("instructions").value as ArrayList<String>)
                             }
-                            override fun onCancelled(error: DatabaseError) {}
-                        })
-                        myIntent = Intent(this@MainActivity, RecipeActivity::class.java)
-                    } else {
-                        Toast.makeText(this@MainActivity,
-                            "No favorite recipe. Please pick another option.",
-                            Toast.LENGTH_LONG).show()
-                    }
+                        }
+                        override fun onCancelled(error: DatabaseError) {}
+                    })
+                    intent = Intent(this@MainActivity, RecipeActivity::class.java)
+                } else {
+                    Toast.makeText(this@MainActivity,
+                        "No favorite recipe. Please pick another option.",
+                        Toast.LENGTH_LONG).show()
                 }
             }
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // write code to perform some action
-            }
         }
-        start.setOnClickListener {
-            startActivity(myIntent)
+
+        override fun onNothingSelected(p0: AdapterView<*>?) {
+            TODO("Not yet implemented")
         }
+
     }
 }
