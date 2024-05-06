3f12f99a32dcd93b8b75480fa3f4f8ba26fdc2cd
Roshni Kainthan <113396273+roshnikai@users.noreply.github.com>
2024-05-06 18:22:16 -0400

    change rating to float

--- RecipeActivity.kt
+++ RecipeActivity.kt
@@ -55,7 +55,7 @@ class RecipeActivity : AppCompatActivity() {
 
     inner class RatingListener: OnRatingBarChangeListener {
         override fun onRatingChanged(ratingBar: RatingBar?, rating: Float, fromUser: Boolean) {
-            currentRecipe.setRating(rating.toInt())
+            currentRecipe.setRating(rating)
         }
     }
     companion object {
